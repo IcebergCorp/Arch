9292eaf73c77ab0f3396675dc2f7155b81b13a96
IcebergCorp <66701150+IcebergCorp@users.noreply.github.com>
2020-06-10 02:36:24 +0300

    Update andr.sh

--- andr.sh
+++ andr.sh
@@ -64,48 +64,4 @@ pacstrap /mnt base base-devel
 genfstab -pU /mnt >> /mnt/etc/fstab
 
 # Заходим в папку под рутом
-arch-chroot /mnt
-
-read -p "Введите имя компьютера: " hostname
-
-echo 'Прописываем имя компьютера'
-echo $hostname > /etc/hostname
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-
-echo '3.4 Добавляем русскую локаль системы'
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
-
-echo 'Обновим текущую локаль системы'
-locale-gen
-
-echo 'Указываем язык системы'
-echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
-
-echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
-echo 'KEYMAP=ru' >> /etc/vconsole.conf
-echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
-
-echo 'Создадим загрузочный RAM диск'
-mkinitcpio -p linux
-
-echo '3.5 Устанавливаем загрузчик'
-pacman -Syy
-pacman -S grub --noconfirm
-grub-install /dev/sda
-
-echo 'Обновляем grub.cfg'
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo 'Ставим программу для Wi-fi'
-pacman -S dialog wpa_supplicant --noconfirm 
-
-echo 'Создаем root пароль'
-passwd
-
-# обновляем пакман
-pacman -Syy
-
-exit
-
-reboot
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
