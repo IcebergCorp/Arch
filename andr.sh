f1afca85c44c1f21cb6f2239f18215edbd143ffd
IcebergCorp <66701150+IcebergCorp@users.noreply.github.com>
2020-06-10 01:25:02 +0300

    Update andr.sh

--- andr.sh
+++ andr.sh
@@ -1,5 +1,19 @@
+#!/bin/bash
+
+# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch2018
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
+
+# В разработке принимали участие:
+# Алексей Бойко https://vk.com/ordanax
+# Степан Скрябин https://vk.com/zurg3
+# Михаил Сарвилин https://vk.com/michael170707
+# Данил Антошкин https://vk.com/danil.antoshkin
+# Юрий Порунцов https://vk.com/poruncov
+
 loadkeys ru
 setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
 
 echo '2.3 Синхронизация системных часов'
 timedatectl set-ntp true
@@ -12,19 +26,19 @@ echo '2.4 создание разделов'
   echo;
   echo;
   echo;
-  echo +512M;
+  echo +100M;
 
   echo n;
   echo;
   echo;
   echo;
-  echo +30G;
+  echo +20G;
 
   echo n;
   echo;
   echo;
   echo;
-  echo +16.5G;
+  echo +1024M;
 
   echo n;
   echo p;
@@ -34,8 +48,10 @@ echo '2.4 создание разделов'
   echo 1;
 
   echo w;
-  
 ) | fdisk /dev/sda
+
+echo 'Ваша разметка диска'
+fdisk -l
 
 echo '2.4.2 Форматирование дисков'
 mkfs.ext2  /dev/sda1 -L boot
