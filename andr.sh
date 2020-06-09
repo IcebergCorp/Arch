cf5d3776955736dd35d617351c9d1265b2154ba9
IcebergCorp <66701150+IcebergCorp@users.noreply.github.com>
2020-06-10 00:20:26 +0300

    Add files via upload

---
+++ andr.sh
@@ -0,0 +1,40 @@
+loadkeys ru
+setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
+echo '2.4 создание разделов'
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +512M;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +30G;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +16.5G;
+
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+
+  echo w;
+  
+) | fdisk /dev/sda
