a4ad8a56c46b078b11279326ce19914487e0c4d0
IcebergCorp <66701150+IcebergCorp@users.noreply.github.com>
2020-06-10 01:54:36 +0300

    Update andr.sh

--- andr.sh
+++ andr.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch2018
-# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
-
-# В разработке принимали участие:
-# Алексей Бойко https://vk.com/ordanax
-# Степан Скрябин https://vk.com/zurg3
-# Михаил Сарвилин https://vk.com/michael170707
-# Данил Антошкин https://vk.com/danil.antoshkin
-# Юрий Порунцов https://vk.com/poruncov
-
+# Добавляем возможность писать в консоли на русском языке
 loadkeys ru
-setfont cyr-sun16
-echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
-echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
 
-echo '2.3 Синхронизация системных часов'
+# Устанавливаем русский шрифт
+setfont cyr-sun16
+
+# Синхронизация системных часов
 timedatectl set-ntp true
 
-echo '2.4 создание разделов'
+# создание разделов
 (
   echo o;
 
@@ -26,19 +17,19 @@ echo '2.4 создание разделов'
   echo;
   echo;
   echo;
-  echo +100M;
+  echo +512M;
 
   echo n;
   echo;
   echo;
   echo;
-  echo +20G;
+  echo +30G;
 
   echo n;
   echo;
   echo;
   echo;
-  echo +1024M;
+  echo +16.5G;
 
   echo n;
   echo p;
@@ -50,28 +41,25 @@ echo '2.4 создание разделов'
   echo w;
 ) | fdisk /dev/sda
 
-echo 'Ваша разметка диска'
-fdisk -l
-
-echo '2.4.2 Форматирование дисков'
+# Форматирование дисков
 mkfs.ext2  /dev/sda1 -L boot
 mkfs.ext4  /dev/sda2 -L root
 mkswap /dev/sda3 -L swap
 mkfs.ext4  /dev/sda4 -L home
 
-echo '2.4.3 Монтирование дисков'
+# Монтирование дисков
 mount /dev/sda2 /mnt
 mkdir /mnt/{boot,home}
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda3
 mount /dev/sda4 /mnt/home
 
-echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
+# Установка зеркала для загрузки ядра.
 echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 
-echo '3.2 Установка основных пакетов'
+# Установка основных пакетов ядра
 pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 
-echo '3.3 Настройка системы'
+# Добавления файоа настроек системы
 genfstab -pU /mnt >> /mnt/etc/fstab
 
