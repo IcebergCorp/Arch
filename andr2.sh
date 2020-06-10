7590879981d32b7da9e0fef349b72fb39da2257d
IcebergCorp <66701150+IcebergCorp@users.noreply.github.com>
2020-06-10 03:12:40 +0300

    Update andr2.sh

--- andr2.sh
+++ andr2.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 read -p "Введите имя компьютера: " hostname
 
-echo 'Прописываем имя компьютера'
+# Прописываем имя компьютера'
 echo $hostname > /etc/hostname
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc
 
-echo '3.4 Добавляем русскую локаль системы'
+# Добавляем русскую локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
 
-echo 'Обновим текущую локаль системы'
+# Обновим текущую локаль системы'
 locale-gen
 
-echo 'Указываем язык системы'
-echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
+# Указываем язык системы'
+echo 'LANG=ru_RU.UTF-8' > /etc/locale.conf
 
-echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+# расскладка клавиатуры
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-echo 'Создадим загрузочный RAM диск'
+# Создадим загрузочный RAM диск'
 mkinitcpio -p linux
 
-echo '3.5 Устанавливаем загрузчик'
+# Устанавливаем загрузчик'
 pacman -Syy
-pacman -S grub --noconfirm
+pacman -S grub --noconfirm intel-ucode
 grub-install /dev/sda
 
-echo 'Обновляем grub.cfg'
+# Обновляем grub.cfg'
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo 'Ставим программу для Wi-fi'
+# Ставим программу для Wi-fi'
 pacman -S dialog wpa_supplicant --noconfirm 
 
-echo 'Создаем root пароль'
+# Создаем root пароль'
 passwd
+
+# Устанавливаем SUDO'
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+# Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 
 # обновляем пакман
 pacman -Syy
 
 exit
-
-reboot
